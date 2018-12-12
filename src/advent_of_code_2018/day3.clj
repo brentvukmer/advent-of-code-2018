0f59b094565897fae34788e7eaa0713d5b910352
Brent Vukmer <brent.vukmer@gmail.com>
2018-12-11 22:02:32 -0500

    Messing about with cgrand's interval-map for 3-1

--- src/advent_of_code_2018/day3.clj
+++ src/advent_of_code_2018/day3.clj
@@ -40,6 +40,14 @@
     (->> (map parse-input-row (line-seq rdr))
          vec)))
 
+(defn convert-row
+  "Takes the raw row data as input and returns the x and y for the corners."
+  [r]
+  (-> r
+      (dissoc :left :top :width :height)
+      (assoc :x [(:left r) (+ (:width r) (:left r))])
+      (assoc :y [(:top r) (+ (:top r) (:height r))])))
+
 ;
 ;
 ; Use a sweep line algorithm along with an interval tree
@@ -67,8 +75,67 @@
 ;    See:
 ;      - https://en.wikipedia.org/wiki/Interval_tree
 ;      - http://www.dgp.toronto.edu/people/JamesStewart/378notes/22intervals/
-;      - http://clj-me.cgrand.net/2012/03/16/a-poor-mans-interval-tree/
 ;
+
+;
+; "Poor man's interval tree"
+; Taken from http://clj-me.cgrand.net/2012/03/16/a-poor-mans-interval-tree/
+;
+(defn interval-lt
+  [[a b] [c d]]
+  (boolean (and b c
+                (if (= a b)
+                  (neg? (compare b c))
+                  (<= (compare b c) 0)))))
+
+(def empty-interval-map
+  (sorted-map-by interval-lt [nil nil] #{}))
+
+(defn- isplit-at [interval-map x]
+  (if x
+    (let [[[a b :as k] vs] (find interval-map [x x])]
+      (if (or (= a x) (= b x))
+        interval-map
+        (-> interval-map (dissoc k) (assoc [a x] vs [x b] vs))))
+    interval-map))
+
+(defn- ialter [interval-map from to f & args]
+  (let [interval-map (-> interval-map (isplit-at from) (isplit-at to))
+        kvs (for [[r vs]
+                  (cond
+                    (and from to)
+                    (subseq interval-map >= [from from] < [to to])
+                    from
+                    (subseq interval-map >= [from from])
+                    to
+                    (subseq interval-map < [to to])
+                    :else
+                    interval-map)]
+              [r (apply f vs args)])]
+    (into interval-map kvs)))
+
+(defn iassoc [interval-map from to v]
+  (ialter interval-map from to conj v))
+
+(defn idissoc [interval-map from to v]
+  (ialter interval-map from to disj v))
+
+(defn iget [interval-map x]
+  (get interval-map [x x]))
+
+(defn build-interval-map
+  [data k]
+  (let [rows (map convert-row data)])
+  (reduce (fn [iv-map row]
+            (let [vals (k row)]
+              (iassoc iv-map (first vals) (inc (last vals)) row)))
+          empty-interval-map
+          rows))
+
+(defn axis-range
+  [p k]
+  (let [c (k p)]
+    (apply sorted-set (range (first c) (inc (second c))))))
 
 ;
 ; SWEEP LINE
@@ -83,6 +150,13 @@
 ;   - Deal with events that occur at the line leaving a solved problem behind.
 ;
 
+(defn claim-intersections
+  [ivmap x y]
+  (filter #(some #{y} (axis-range % :y)) (iget ivmap x)))
+
+;
+; Total claim intersection area
+;
 
 (defn area
   [overlap]
@@ -168,6 +242,9 @@
                :features [:resizable])
 
   (def raw-data (read-raw-data "day3"))
+  (def x-ivmap (build-interval-map raw-data :x))
+  (def y-ivmap (build-interval-map raw-data :y))
+
   (def sorted-data (sort-by (juxt :left :top) raw-data))
   (def sample (take 1000 sorted-data))
   (q/defsketch day3-sketch
@@ -184,22 +261,10 @@
 ;
 ;
 (comment
-  (defn convert-row
-    "Takes the raw row data as input and returns the x and y for the corners."
-    [r]
-    (-> r
-        (dissoc :left :top :width :height)
-        (assoc :x [(:left r) (+ (:width r) (:left r))])
-        (assoc :y [(:top r) (+ (:top r) (:height r))])))
 
   (defn read-data
     [path]
     (map convert-row (read-raw-data path)))
-
-  (defn- axis-range
-    [p k]
-    (let [c (k p)]
-      (apply sorted-set (range (first c) (inc (second c))))))
 
   (defn- x-range
     [p]
