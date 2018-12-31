447eae8a1ee81fef2c3fe4f35e59f7a2cf74a895
Brent Vukmer <brent.vukmer@gmail.com>
2018-12-30 22:44:29 -0500

    Solved 7-2

--- src/advent_of_code_2018/day7.clj
+++ src/advent_of_code_2018/day7.clj
@@ -160,58 +160,77 @@
 (defn process-one-sec
   [{:keys [accumulator workers evaluation-set forward-links back-links time-offset]}]
   (let [done-workers (filter #(done? % time-offset) workers)
+        available-workers (into (sorted-map) (concat done-workers (filter idle? workers)))
+        ; Update busy workers with the step that they're already working on.
+        busy-workers (->> (apply dissoc workers (keys available-workers))
+                          (map #(vector (first %) (conj (second %) (last (second %)))))
+                          (into (sorted-map)))
+        steps-in-progress (map #(last (second %)) busy-workers)
         completed-steps (map #(last (second %)) done-workers)
         updated-accumulator (apply conj accumulator completed-steps)
-        updated-eval-set (apply disj evaluation-set completed-steps)
-        next-steps (->> (filter #(let [requirements (set (% back-links))]
-                                   (or (nil? requirements)
-                                       (= requirements
-                                          (set/intersection requirements (set updated-accumulator))))) updated-eval-set)
-                        sort)]
-    (if (seq next-steps)
-      (let [available-workers (into (sorted-map) (concat done-workers (filter idle? workers)))
-            ; Update busy workers with the step that they're already working on.
-            busy-workers (->> (apply dissoc workers (keys available-workers))
-                              (map #(vector (first %) (conj (second %) (last (second %)))))
-                              (into (sorted-map)))
-            new-steps (take (count available-workers) next-steps)
-            ; Update assigned workers with new steps.
-            assigned-workers (->> (into (sorted-map) (take (count new-steps) available-workers))
-                                  (map vector new-steps)
-                                  (map #(vector (first (second %)) (conj (second (second %)) (first %))))
-                                  (into {}))
-            ; Update unused workers with nil at the end of the queue.
-            unused-workers (->> (into {} (take-last (count new-steps) available-workers))
-                                (map #(vector (first %) (conj (second %) nil)))
-                                (into {}))]
-        {:accumulator    updated-accumulator
-         :workers        (conj busy-workers assigned-workers unused-workers)
-         :evaluation-set (set/union updated-eval-set
-                                    (apply sorted-set (apply concat (map forward-links new-steps))))
-         :forward-links  forward-links
-         :back-links     back-links
-         :time-offset    time-offset})
-      {:accumulator    updated-accumulator
-       :workers        workers
-       :evaluation-set updated-eval-set
-       :forward-links  forward-links
-       :back-links     back-links
-       :time-offset    time-offset}))
+        updated-eval-set (if (empty? evaluation-set)
+                           (apply sorted-set (set/difference (set (keys forward-links)) (set (keys back-links))))
+                           (if (not= accumulator updated-accumulator)
+                             (set/union (apply disj evaluation-set completed-steps)
+                                        (apply sorted-set (apply concat (map forward-links completed-steps))))
+                             evaluation-set))
+        next-steps (if (not= evaluation-set updated-eval-set)
+                     (->> (filter #(let [requirements (set (% back-links))]
+                                     (or (nil? requirements)
+                                         (= requirements
+                                            (set/intersection requirements (set updated-accumulator))))) updated-eval-set)
+                          (remove #(contains? (set steps-in-progress) %))
+                          sort)
+                     '())
+        new-steps (take (count available-workers) next-steps)
+        ; Update assigned workers with new steps.
+        assigned-workers (->> (into (sorted-map) (take (count new-steps) available-workers))
+                              (map vector new-steps)
+                              (map #(vector (first (second %)) (conj (second (second %)) (first %))))
+                              (into {}))
+        ; Update unused workers with nil at the end of the queue.
+        unused-workers (->> (into {} (apply dissoc available-workers (keys assigned-workers)))
+                            (map #(vector (first %) (conj (second %) nil)))
+                            (into {}))
+        updated-workers (if (seq next-steps)
+                          (conj busy-workers assigned-workers unused-workers)
+                          (conj busy-workers unused-workers))]
+    ;(println "done-workers: " done-workers)
+    ;(println "available-workers: " available-workers)
+    ;(println "busy-workers: " busy-workers)
+    ;(println "completed-steps: " completed-steps)
+    ;(println "updated-accumulator: " updated-accumulator)
+    ;(println "updated-eval-set: " updated-eval-set)
+    ;(println "next-steps: " next-steps)
+    ;(println "new-steps: " new-steps)
+    ;(println "assigned-workers: " assigned-workers)
+    ;(println "unused-workers: " unused-workers)
+    {:accumulator    updated-accumulator
+     :workers        updated-workers
+     :evaluation-set updated-eval-set
+     :forward-links  forward-links
+     :back-links     back-links
+     :time-offset    time-offset})
   )
 
 (defn part2
-  [data num-workers time-offset]
-  (let [back-links (links data second first)
-        forward-links (links data first second)
-        start-set (apply sorted-set (set/difference (set (keys forward-links)) (set (keys back-links))))]
-    ;(take-while #(seq (:evaluation-set %))
-    ;            )
-    (take 9 (iterate process-one-sec {:accumulator    []
-                                      :workers        (->> (take num-workers (repeat []))
-                                                           (map vector (range 1 (inc num-workers)))
-                                                           (into {}))
-                                      :evaluation-set start-set
-                                      :forward-links  forward-links
-                                      :back-links     back-links
-                                      :time-offset    time-offset}))))
+  ([data num-workers time-offset]
+   (let [back-links (links data second first)
+         forward-links (links data first second)
+         seconds-states
+         (->> (take-while #(not= (set (flatten (seq forward-links))) (set (:accumulator %)))
+                          (iterate process-one-sec {:accumulator    []
+                                                    :workers        (->> (take num-workers (repeat []))
+                                                                         (map vector (range 1 (inc num-workers)))
+                                                                         (into {}))
+                                                    :evaluation-set #{}
+                                                    :forward-links  forward-links
+                                                    :back-links     back-links
+                                                    :time-offset    time-offset}))
+              vec)]
+     (->> (conj seconds-states (process-one-sec (last seconds-states)))
+          count
+          ((comp dec dec)))))
+  ([]
+    (part2 (read-raw-data "day7") 5 60)))
 
--- test/advent_of_code_2018/day7_test.clj
+++ test/advent_of_code_2018/day7_test.clj
@@ -2,62 +2,150 @@
   (:require [clojure.test :refer :all]
             [advent-of-code-2018.day7 :refer :all]))
 
+;Second   Worker 1   Worker 2   Done
+;0        C          .
+;1        C          .
+;2        C          .
+;3        A          F       C
+;4        B          F       CA
+;5        B          F       CA
+;6        D          F       CAB
+;7        D          F       CAB
+;8        D          F       CAB
+;9        D          .       CABF
+;10        E          .       CABFD
+;11        E          .       CABFD
+;12        E          .       CABFD
+;13        E          .       CABFD
+;14        E          .       CABFD
+;15        .          .       CABFDE
+
+(def test-states [{:accumulator [],
+              :workers          {1 [], 2 []},
+              :evaluation-set   #{},
+              :forward-links    {:C '(:A :F), :A '(:B :D), :B '(:E), :D '(:E), :F '(:E)},
+              :back-links       {:A '(:C), :F '(:C), :B '(:A), :D '(:A), :E '(:B :D :F)},
+              :time-offset      0},
+             {:accumulator    [],
+              :workers        {1 [:C], 2 [nil]},
+              :evaluation-set #{:C},
+              :forward-links  {:C '(:A :F), :A '(:B :D), :B '(:E), :D '(:E), :F '(:E)},
+              :back-links     {:A '(:C), :F '(:C), :B '(:A), :D '(:A), :E '(:B :D :F)},
+              :time-offset    0},
+             {:accumulator    [],
+              :workers        {1 [:C :C], 2 [nil nil]},
+              :evaluation-set #{:C},
+              :forward-links  {:C '(:A :F), :A '(:B :D), :B '(:E), :D '(:E), :F '(:E)},
+              :back-links     {:A '(:C), :F '(:C), :B '(:A), :D '(:A), :E '(:B :D :F)},
+              :time-offset    0},
+             {:accumulator    [],
+              :workers        {1 [:C :C :C], 2 [nil nil nil]},
+              :evaluation-set #{:C},
+              :forward-links  {:C '(:A :F), :A '(:B :D), :B '(:E), :D '(:E), :F '(:E)},
+              :back-links     {:A '(:C), :F '(:C), :B '(:A), :D '(:A), :E '(:B :D :F)},
+              :time-offset    0},
+             {:accumulator    [:C],
+              :workers        {1 [:C :C :C :A], 2 [nil nil nil :F]},
+              :evaluation-set #{:A :F},
+              :forward-links  {:C '(:A :F), :A '(:B :D), :B '(:E), :D '(:E), :F '(:E)},
+              :back-links     {:A '(:C), :F '(:C), :B '(:A), :D '(:A), :E '(:B :D :F)},
+              :time-offset    0},
+             {:accumulator    [:C :A],
+              :workers        {1 [:C :C :C :A :B], 2 [nil nil nil :F :F]},
+              :evaluation-set #{:B :D :F},
+              :forward-links  {:C '(:A :F), :A '(:B :D), :B '(:E), :D '(:E), :F '(:E)},
+              :back-links     {:A '(:C), :F '(:C), :B '(:A), :D '(:A), :E '(:B :D :F)},
+              :time-offset    0},
+             {:accumulator    [:C :A],
+              :workers        {1 [:C :C :C :A :B :B], 2 [nil nil nil :F :F :F]},
+              :evaluation-set #{:B :D :F},
+              :forward-links  {:C '(:A :F), :A '(:B :D), :B '(:E), :D '(:E), :F '(:E)},
+              :back-links     {:A '(:C), :F '(:C), :B '(:A), :D '(:A), :E '(:B :D :F)},
+              :time-offset    0},
+             {:accumulator    [:C :A :B],
+              :workers        {1 [:C :C :C :A :B :B :D], 2 [nil nil nil :F :F :F :F]},
+              :evaluation-set #{:D :E :F},
+              :forward-links  {:C '(:A :F), :A '(:B :D), :B '(:E), :D '(:E), :F '(:E)},
+              :back-links     {:A '(:C), :F '(:C), :B '(:A), :D '(:A), :E '(:B :D :F)},
+              :time-offset    0},
+             {:accumulator    [:C :A :B],
+              :workers        {1 [:C :C :C :A :B :B :D :D], 2 [nil nil nil :F :F :F :F :F]},
+              :evaluation-set #{:D :E :F},
+              :forward-links  {:C '(:A :F), :A '(:B :D), :B '(:E), :D '(:E), :F '(:E)},
+              :back-links     {:A '(:C), :F '(:C), :B '(:A), :D '(:A), :E '(:B :D :F)},
+              :time-offset    0},
+             {:accumulator    [:C :A :B],
+              :workers        {1 [:C :C :C :A :B :B :D :D :D], 2 [nil nil nil :F :F :F :F :F :F :F]},
+              :evaluation-set #{:D :E :F},
+              :forward-links  {:C '(:A :F), :A '(:B :D), :B '(:E), :D '(:E), :F '(:E)},
+              :back-links     {:A '(:C), :F '(:C), :B '(:A), :D '(:A), :E '(:B :D :F)},
+              :time-offset    0},
+             {:accumulator    [:C :A :B :F],
+              :workers        {1 [:C :C :C :A :B :B :D :D :D :D], 2 [nil nil nil :F :F :F :F :F :F nil]},
+              :evaluation-set #{:D :E},
+              :forward-links  {:C '(:A :F), :A '(:B :D), :B '(:E), :D '(:E), :F '(:E)},
+              :back-links     {:A '(:C), :F '(:C), :B '(:A), :D '(:A), :E '(:B :D :F)},
+              :time-offset    0},
+             {:accumulator    [:C :A :B :F :D],
+              :workers        {1 [:C :C :C :A :B :B :D :D :D :D :E], 2 [nil nil nil :F :F :F :F :F :F nil nil]},
+              :evaluation-set #{:E},
+              :forward-links  {:C '(:A :F), :A '(:B :D), :B '(:E), :D '(:E), :F '(:E)},
+              :back-links     {:A '(:C), :F '(:C), :B '(:A), :D '(:A), :E '(:B :D :F)},
+              :time-offset    0},
+             {:accumulator    [:C :A :B :F :D],
+              :workers        {1 [:C :C :C :A :B :B :D :D :D :D :E :E], 2 [nil nil nil :F :F :F :F :F :F nil nil nil]},
+              :evaluation-set #{:E},
+              :forward-links  {:C '(:A :F), :A '(:B :D), :B '(:E), :D '(:E), :F '(:E)},
+              :back-links     {:A '(:C), :F '(:C), :B '(:A), :D '(:A), :E '(:B :D :F)},
+              :time-offset    0},
+             {:accumulator    [:C :A :B :F :D],
+              :workers        {1 [:C :C :C :A :B :B :D :D :D :D :E :E :E], 2 [nil nil nil :F :F :F :F :F :F nil nil nil nil]},
+              :evaluation-set #{:E},
+              :forward-links  {:C '(:A :F), :A '(:B :D), :B '(:E), :D '(:E), :F '(:E)},
+              :back-links     {:A '(:C), :F '(:C), :B '(:A), :D '(:A), :E '(:B :D :F)},
+              :time-offset    0},
+             {:accumulator    [:C :A :B :F :D],
+              :workers        {1 [:C :C :C :A :B :B :D :D :D :D :E :E :E :E], 2 [nil nil nil :F :F :F :F :F :F nil nil nil nil nil]},
+              :evaluation-set #{:E},
+              :forward-links  {:C '(:A :F), :A '(:B :D), :B '(:E), :D '(:E), :F '(:E)},
+              :back-links     {:A '(:C), :F '(:C), :B '(:A), :D '(:A), :E '(:B :D :F)},
+              :time-offset    0},
+             {:accumulator    [:C :A :B :F :D],
+              :workers        {1 [:C :C :C :A :B :B :D :D :D :D :E :E :E :E :E], 2 [nil nil nil :F :F :F :F :F :F nil nil nil nil nil nil]},
+              :evaluation-set #{:E},
+              :forward-links  {:C '(:A :F), :A '(:B :D), :B '(:E), :D '(:E), :F '(:E)},
+              :back-links     {:A '(:C), :F '(:C), :B '(:A), :D '(:A), :E '(:B :D :F)},
+              :time-offset    0},
+             {:accumulator    [:C :A :B :F :D :E],
+              :workers        {1 [:C :C :C :A :B :B :D :D :D :D :E :E :E :E :E nil], 2 [nil nil nil :F :F :F :F :F :F nil nil nil nil nil nil nil]},
+              :evaluation-set #{},
+              :forward-links  {:C '(:A :F), :A '(:B :D), :B '(:E), :D '(:E), :F '(:E)},
+              :back-links     {:A '(:C), :F '(:C), :B '(:A), :D '(:A), :E '(:B :D :F)},
+              :time-offset    0}])
+
 (deftest step-zero
-  (testing "The output for step zero is the correct input for step one."
-    (is (= {:accumulator [],
-            :workers {1 [:C], 2 [nil]},
-            :evaluation-set #{:A :C :F},
-            :forward-links {:C '(:A :F), :A '(:B :D), :B '(:E), :D '(:E), :F '(:E)},
-            :back-links {:A '(:C), :F '(:C), :B '(:A), :D '(:A), :E '(:B :D :F)},
-            :time-offset 0}
-           (process-one-sec {:accumulator [],
-                             :workers {1 [], 2 []},
-                             :evaluation-set #{:C},
-                             :forward-links {:C '(:A :F), :A '(:B :D), :B '(:E), :D '(:E), :F '(:E)},
-                             :back-links {:A '(:C), :F '(:C), :B '(:A), :D '(:A), :E '(:B :D :F)},
-                             :time-offset 0})))))
+  (testing "The output for second zero is the correct input for second one."
+    (is (= (process-one-sec (first test-states)) (second test-states)))))
 
 (deftest step-one
-  (testing "The output for step one is the correct input for step two."
-    (is (= {:accumulator [],
-            :workers {1 [:C :C], 2 [nil nil]},
-            :evaluation-set #{:A :B :C :D :E :F},
-            :forward-links {:C '(:A :F), :A '(:B :D), :B '(:E), :D '(:E), :F '(:E)},
-            :back-links {:A '(:C), :F '(:C), :B '(:A), :D '(:A), :E '(:B :D :F)},
-            :time-offset 0}
-           (process-one-sec {:accumulator [],
-                             :workers {1 [:C], 2 [nil]},
-                             :evaluation-set #{:A :C :F},
-                             :forward-links {:C '(:A :F), :A '(:B :D), :B '(:E), :D '(:E), :F '(:E)},
-                             :back-links {:A '(:C), :F '(:C), :B '(:A), :D '(:A), :E '(:B :D :F)},
-                             :time-offset 0})))))
+  (testing "The output for second one is the correct input for second two."
+    (is (= (process-one-sec (second test-states))
+           (nth test-states 2)))))
 
 (deftest step-two
-  (testing "The output for step two is the correct input for step three."
-    (is (= {:accumulator [],
-            :workers {1 [:C :C :C], 2 [nil nil nil]},
-            :evaluation-set #{:A :B :C :D :E :F},
-            :forward-links {:C '(:A :F), :A '(:B :D), :B '(:E), :D '(:E), :F '(:E)},
-            :back-links {:A '(:C), :F '(:C), :B '(:A), :D '(:A), :E '(:B :D :F)},
-            :time-offset 0}
-           (process-one-sec {:accumulator [],
-                             :workers {1 [:C :C], 2 [nil nil]},
-                             :evaluation-set #{:A :C :F},
-                             :forward-links {:C '(:A :F), :A '(:B :D), :B '(:E), :D '(:E), :F '(:E)},
-                             :back-links {:A '(:C), :F '(:C), :B '(:A), :D '(:A), :E '(:B :D :F)},
-                             :time-offset 0})))))
+  (testing "The output for second two is the correct input for second three."
+    (is (= (process-one-sec (nth test-states 2))
+           (nth test-states 3)))))
 
 (deftest step-three
-  (testing "The output for step three is the correct input for step four."
-    (is (= {:accumulator [:C],
-            :workers {1 [:C :C :C :A], 2 [nil nil nil nil]},
-            :evaluation-set #{:A :B :D :E :F},
-            :forward-links {:C '(:A :F), :A '(:B :D), :B '(:E), :D '(:E), :F '(:E)},
-            :back-links {:A '(:C), :F '(:C), :B '(:A), :D '(:A), :E '(:B :D :F)},
-            :time-offset 0}
-           (process-one-sec {:accumulator [],
-                             :workers {1 [:C :C :C], 2 [nil nil nil]},
-                             :evaluation-set #{:A :C :F},
-                             :forward-links {:C '(:A :F), :A '(:B :D), :B '(:E), :D '(:E), :F '(:E)},
-                             :back-links {:A '(:C), :F '(:C), :B '(:A), :D '(:A), :E '(:B :D :F)},
-                             :time-offset 0})))))
+  (testing "The output for second three is the correct input for second four."
+    (is (= (process-one-sec (nth test-states 3))
+           (nth test-states 4)))))
+
+(deftest step-four
+  (testing "The output for second four is the correct input for second five."
+    (is (= (process-one-sec (nth test-states 4))
+           (nth test-states 5)))))
+
+(deftest state-pairs
+  (testing "The output"))
