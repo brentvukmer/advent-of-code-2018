d7b59d9a55c19eb20f3d49cfec1c55a4f044c579
brent-vukmer <brent.vukmer@capitalone.com>
2019-01-04 15:01:04 -0500

    Massive speedup for 9-2 using Brownies Collections (IntBigList, specifically.)

--- src/java/aoc/day9/CircleGameSolver.java
+++ src/java/aoc/day9/CircleGameSolver.java
@@ -1,51 +1,52 @@
 package aoc.day9;
 
 import clojure.lang.Keyword;
+import org.magicwerk.brownies.collections.primitive.IntBigList;
 
-import java.util.ArrayList;
+import java.math.BigInteger;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+import static java.math.BigInteger.ZERO;
+import static java.math.BigInteger.valueOf;
+
 public class CircleGameSolver {
 
-    private static boolean shouldTakeMarbles(int m) {
-        return (m > 0) && (m % 23) == 0;
-    }
+  private static boolean shouldTakeMarbles(int m) {
+    return (m > 0) && (m % 23) == 0;
+  }
 
-    public static Map<Keyword, Object> solveFor(int numPlayers, int maxMarbleValue) {
-        List<Integer> circle = new ArrayList<>(maxMarbleValue + 1);
-        circle.add(0);
-        Map<Integer, Integer> playerScore = new HashMap<>(numPlayers);
-        int currentIndex = 0;
-        int bound = maxMarbleValue + 1;
-        for (int m = 1; m < bound; m++) {
-            if (shouldTakeMarbles(m)) {
-                int player = m % numPlayers;
-                int removeIndex = Math.abs((currentIndex - 7) % circle.size());
-                int removedMarble = circle.get(removeIndex);
-                circle.remove(removeIndex);
-                playerScore.putIfAbsent(player, 0);
-                Integer score = playerScore.get(player);
-                playerScore.put(player, score + m + removedMarble);
-                currentIndex = removeIndex;
-            } else {
-                int nextIndex = ((currentIndex + 1) % circle.size()) + 1;
-                circle.add(nextIndex, m);
-                currentIndex = nextIndex;
-            }
-        }
-        Map<Keyword, Object> result = new HashMap<>();
-        result.put(Keyword.intern(null, "circle"), circle);
-        List<Integer> gameScores = playerScore
-                .values()
-                .stream()
-                .sorted()
-                .collect(Collectors.toList());
-        result.put(Keyword.intern(null, "high-score"), gameScores.get(gameScores.size() - 1));
-        result.put(Keyword.intern(null, "player-marbles"), playerScore);
-        return result;
+  public static Map<Keyword, Object> solveFor(int numPlayers, int maxMarbleValue) {
+    IntBigList circle = new IntBigList();
+    circle.add(0);
+    Map<Integer, BigInteger> playerScore = new HashMap<>(numPlayers);
+    int currentIndex = 0;
+    int bound = maxMarbleValue + 1;
+    for (int m = 1; m < bound; m++) {
+      if (shouldTakeMarbles(m)) {
+        int player = m % numPlayers;
+        int removeIndex = Math.abs((currentIndex - 7) % circle.size());
+        int removedMarble = circle.get(removeIndex);
+        circle.remove(removeIndex);
+        playerScore.putIfAbsent(player, ZERO);
+        BigInteger updatedScore =
+            playerScore.get(player).add(valueOf(m)).add(valueOf(removedMarble));
+        playerScore.put(player, updatedScore);
+        currentIndex = removeIndex;
+      } else {
+        int nextIndex = ((currentIndex + 1) % circle.size()) + 1;
+        circle.add(nextIndex, m);
+        currentIndex = nextIndex;
+      }
     }
-
+    Map<Keyword, Object> result = new HashMap<>();
+    result.put(Keyword.intern(null, "circle"), circle);
+    List<BigInteger> gameScores =
+        playerScore.values().stream().sorted().collect(Collectors.toList());
+    result.put(Keyword.intern(null, "high-score"), gameScores.get(gameScores.size() - 1));
+    result.put(Keyword.intern(null, "player-marbles"), playerScore);
+    return result;
+  }
 }
